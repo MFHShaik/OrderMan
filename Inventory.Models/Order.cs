6d50b243a71244c3422385fd4e36cbde306403ec
Mohammed <fuzailshaik22@gmail.com>
2024-03-30 21:22:13 +0200

    Uploading Files

---
+++ Inventory.Models/Order.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Order
+    {
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public Ordertype Ordertype { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+
+        public decimal SubTotal { get; set; }
+        public decimal ItemDiscounted { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public decimal ShippingCharges { get; set; }
+
+        public decimal Total { get; set; }
+
+        public string PromoCode { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public decimal GrandTotal { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Description { get; set; }
+        public DateTime UpdatedAt { get; set; } 
+
+    }
+}
+
+namespace Inventory.Models
+{
+    public enum OrderStatus
+    {
+        CheckOut,Paid,Failed,Shipped,Delivered, Returned, Completed
+    }
+}
+
+namespace Inventory.Models
+{
+    public enum Ordertype
+    {
+        PurchaseOrder, CustomerOrder
+    }
+}
